75f173c8bb40f1f88c7b7324f9361c10b86a1782
Tomasz Sobutka <tomaszsobutka@gmail.com>
2020-04-11 18:02:17 +0200

    Poprawa wzorca Observer 2

--- src/main/java/com/example/demo/ObserverApplication.java
+++ src/main/java/com/example/demo/ObserverApplication.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import observator.*;
 
-@SpringBootApplication
 public class ObserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ObserverApplication.class, args);
+		Observable<WeatherStation> observable = new WeatherStation();
+		IObserver<WeatherStation> printDisplay = new PrintDisplay();
+		IObserver<WeatherStation> prettyPrintDisplay = new PrettyPrintDisplay();
+		observable.add(printDisplay);
+		observable.add(prettyPrintDisplay);
+		observable.getObservable().setTemperature(14);
 	}
 
 }
